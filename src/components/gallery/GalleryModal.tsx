fd81c04d5c9d1c2c559f5714592b046cc861fc92
GovindSinghsp <160107220+GovindSinghsp@users.noreply.github.com>
2025-11-27 12:19:23 +0530

    Updated GalleryModal.tsx

--- src/components/gallery/GalleryModal.tsx
+++ src/components/gallery/GalleryModal.tsx
@@ -41,7 +41,7 @@ const GalleryModal: React.FC<GalleryModalProps> = ({ isOpen, onClose, item }) =>
 
   return (
     <div className="fixed inset-0 bg-black/90 z-50 flex items-center justify-center p-4">
-      <div className="relative w-[85%] h-[85%] max-w-7xl">
+      <div className="relative w-[75%] h-[75%] max-w-7xl">
         {/* Close Button */}
         <button
           onClick={onClose}
